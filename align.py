6704c704c29281cf6fc066826eaa012c5d73c113
Joseph Kim <jjkim573@gmail.com>
2026-03-31 11:57:32 -0700

    change file input from 1 fasta file to 2

--- align.py
+++ align.py
@@ -1,11 +1,12 @@
-from pathlib import Path
 import numpy as np
+import argparse
 
-def parse_FASTA(file_path):
+def parse_fasta(file_path):
     """
     Returns sequence of nucleotides after reading FASTA file
     """
 
+    # open file if possible
     try:
         with open(file_path, 'r', encoding='utf-8') as f:
             content = f.read()
@@ -14,17 +15,13 @@ def parse_FASTA(file_path):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-    print(content)
-
+    # split file content into lines
     lines = content.split('\n')
 
-    dna_sequences = []
+    # return dna sequence, not header
     for line in lines:
         if line and line[0] != '>':
-            dna_sequences.append(line)
-
-    return dna_sequences
-
+            return line
 
 def check(a, b):
     match = 1
@@ -49,10 +46,10 @@ def needleman_wunsch(a, b):
     dp_table = np.zeros((num_rows, num_cols))
 
     # initialize first column 
-    dp_table[:, 0] = -1 * np.arange(0, num_rows)
+    dp_table[:, 0] = d * np.arange(0, num_rows)
 
     # initialize first row
-    dp_table[0, :] = -1 * np.arange(0, num_cols)
+    dp_table[0, :] = d * np.arange(0, num_cols)
 
     # fill in dp table
     for i in range(1, num_rows):
@@ -71,6 +68,7 @@ def needleman_wunsch(a, b):
     i = len(a) 
     j = len(b) 
 
+    # retrace optimal path
     while (i > 0 or j > 0):
         # remember that strings don't have padding
         if (i > 0 and j > 0 and dp_table[i, j] == dp_table[i-1, j-1] + check(a[i-1], b[j-1])):
@@ -88,12 +86,10 @@ def needleman_wunsch(a, b):
 
             # update position
             i -= 1
-        else:
+        elif (j > 0 and dp_table[i, j] == dp_table[i, j-1] + d):
             # go left
             alignment_A = '-' + alignment_A
             alignment_B = b[j-1] + alignment_B
-
-            # update position
             j -= 1
 
     optimal_score = dp_table[-1, -1]
@@ -101,16 +97,25 @@ def needleman_wunsch(a, b):
     
 
 def main():
-    BASE_DIR = Path(__file__).resolve().parent # gets directory file is running in
-    file_path = BASE_DIR / "test.fasta"
-    dna_sequences = parse_FASTA(file_path)
+    # initialize parser
+    parser = argparse.ArgumentParser(description="A custom Needleman-Wunsch global sequence aligner.")
+    # positional arguments
+    parser.add_argument("sequence1", help="First FASTA file path")
+    parser.add_argument("sequence2", help="Second FASTA file path")
 
-    dna_1, dna_2 = dna_sequences
+    args = parser.parse_args()
+    file1_path = args.sequence1
+    file2_path = args.sequence2
+
+    # parse the fasta files
+    sequence1 = parse_fasta(file1_path)
+    sequence2 = parse_fasta(file2_path)
     
-    alignment_A, alignment_B, optimal_score = needleman_wunsch(dna_1, dna_2)
+    alignment_A, alignment_B, optimal_score = needleman_wunsch(sequence1, sequence2)
 
-    print('alignment A: ' + alignment_A)
-    print('alignment B: ' + alignment_B)
+    print('alignment 1: ' + alignment_A)
+    print('alignment 2: ' + alignment_B)
     print('optimal score: ' + str(optimal_score))
 
-main()
+if __name__ == "__main__":
+    main()
